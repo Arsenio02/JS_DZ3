8fd8c182eed835ce14a5961e62cb8510a12b1fea
Arsenio02 <128214234+Arsenio02@users.noreply.github.com>
2024-03-10 21:12:04 +0600

    Add files via upload

---
+++ JS_DZ3/script.js
@@ -0,0 +1,30 @@
+// First
+
+/*
+const number = parseInt(prompt("Введите число от 2 до 10: "));
+
+if (number < 2 || number > 10) {
+    alert("Введено недопустимое число!");
+}
+
+for (let i = 1; i <= 10; i++) {
+    const result = number * i;
+    console.log(`${number} × ${i} = ${result}`);
+}
+*/
+
+// Second
+
+/*
+const cards = ["46782346", "45781218", "79874568", "12157845", "36151845", "41250895", "41201961"];
+
+let visaCount = 0;
+
+for (const card of cards) {
+    if (card[0] === "4") {
+        visaCount++;
+    }
+}
+
+console.log(`Карт VISA ${visaCount} из ${cards.length}`);
+*/
